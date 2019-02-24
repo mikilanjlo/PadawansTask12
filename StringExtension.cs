41b51dd1af121fb90abc028fbce01e5568412e97
mikilanjlo <mikilanjlo@yandex.by>
2019-02-24 13:41:47 +0300

    calculate

--- StringExtension.cs
+++ StringExtension.cs
@@ -6,7 +6,30 @@ namespace PadawansTask12
     {
         public static bool AllCharactersAreUnique(string source)
         {
-            throw new NotImplementedException();
+            if (source == null)
+                throw new ArgumentNullException();
+            if (source == "")
+                throw new ArgumentException();
+            char symbol;
+            bool result = true;
+            for (int i = 0; i < 256; i++)
+            {
+                symbol = (char)i;
+                int count = 0;
+                for(int j = 0; j < source.Length; j++)
+                    if(symbol == source[j])
+                    {
+                        count++;
+                        if(count > 1)
+                        {
+                            result = false;
+                            break;
+                        }
+                    }
+                if (!result)
+                    break;
+            }
+            return result;
         }
     }
 }
